d8119cced97b4d749ab906805c26a56608e58a4a
Spandana <spandanarangaswamy21@gmail.com>
2023-11-08 19:17:52 +0530

    login form

---
+++ app/login/page.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import LoginForm from "./login-form";
+
+const LoginPage = () => {
+  return (
+    <div className="grid w-full h-[100vh] grid-cols-1 md:grid-cols-3">
+      <LoginForm />
+      {/* Gradient */}
+      <div className="relative hidden w-full overflow-hidden md:col-span-2 rounded-l-2xl md:block">
+        {/* Overlay */}
+        <div className="absolute inset-0 z-10 bg-white/10 backdrop-blur-lg" />
+        {/* Content */}
+        <div className="absolute inset-0 z-20 flex items-center justify-center px-8">
+          <div>
+            <Image
+              className="max-w-[200px]"
+              width={296}
+              height={77}
+              src="/makr-logo-dark.svg"
+              alt="makr-logo"
+            />
+            <div className="mt-5">
+              <div className="text-3xl font-medium text-neutral-900">
+                Nextgpt
+              </div>
+            </div>
+          </div>
+        </div>
+        <Image
+          priority
+          sizes="50vw"
+          className="z-0"
+          alt="gradient"
+          fill
+          src="/login-gradient.jpg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
